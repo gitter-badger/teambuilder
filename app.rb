79d044184aa095c5934e827dd58e3415779dac9a
azaytsev <azaytsev@ggasoftware.com>
2014-07-16 18:36:11 +0400

    implemented simple JSON api for members

--- app.rb
+++ app.rb
@@ -1,9 +1,51 @@
 require 'sinatra'
+require 'sequel'
+require "json"
+
+
+@db = Sequel.connect('sqlite://members.db')
+
+@db.create_table :members do
+  primary_key :id
+  string      :name
+  string      :team
+  string      :avatar_url
+end rescue nil
+
+class Member < Sequel::Model end
 
 set :public_folder, 'public'
 
 get '/' do
   html :index
+end
+
+get '/members' do
+  content_type :json
+  @members = Member.all
+  @members.map(&:values).to_json
+end
+
+post '/members' do
+  content_type :json
+  params_json = JSON.parse(request.body.read)
+  @member = Member.create(params_json)
+  @member.values.to_json
+end
+
+put '/members/:id' do
+  content_type :json
+  params_json = JSON.parse(request.body.read)
+  @member = Member.find(id: params[:id].to_i)
+  @member.update(params_json)
+  @member.values.to_json
+end
+
+delete '/members/:id' do
+  content_type :json
+  @member = Member.find(id: params[:id].to_i)
+  @member.destroy
+  {:success => "ok"}.to_json
 end
 
 def html(view)
