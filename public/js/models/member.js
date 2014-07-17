361d4886f228e6a88a84dd648d4dac830bc2cb40
own-wave <n.b.kornilov@gmail.com>
2014-07-17 20:18:28 +0400

    member.js update

--- public/js/models/member.js
+++ public/js/models/member.js
@@ -3,5 +3,12 @@ var Member = Backbone.Model.extend({
 		name: '',
 		avatar_url: 'http://',
 		team: ''
+	},
+	initialize: function(){
+		alert('Hello, mr Newby');
+
+		this.on('change: name', function() {
+			console.log('Name has been changed!')
+		})
 	}
 })
